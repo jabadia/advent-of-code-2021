21657524ac6a551d437a58b682e7470cd2febb6a
Javier Abadía <javi.abadia@gmail.com>
2021-12-06 16:53:58 +0100

    completed day 6

---
+++ d06/d6p1.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from utils.test_case import TestCase
+from d6_input import INPUT
+
+TEST_CASES = [
+    TestCase("""
+3,4,3,1,2
+""", 5934),
+]
+
+
+def solve(input):
+    fishes = Counter(int(n) for n in input.strip().split(','))
+
+    for time in range(80):
+        spawn = fishes[0]
+        for cycle in range(8):
+            fishes[cycle] = fishes[cycle + 1]
+        fishes[8] = spawn
+        fishes[6] += spawn
+
+    return sum(fishes.values())
+
+
+if __name__ == '__main__':
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(INPUT))
